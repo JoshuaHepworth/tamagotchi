872f42e928daf5f119efddaccda3134782559867
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-25 07:50:38 -0500

    more pseudo

--- js/app.js
+++ js/app.js
@@ -36,3 +36,75 @@ console.log('tamagotchi')
 // Morph your pet at certain ages.
 
 // Animate your pet across the screen while it's alive.
+
+//------------------Pseudo
+
+// First I should draw my pet on paper to get an idea of what he'll look like.
+
+//add buttons in my HTML for feed, turn off lights, and play with pet.
+
+//Create a class for my tamagotchi with hunger, sleepiness, boredom, and age as its keys
+
+// Create an input box to store the name of the tamagotchi instince
+
+//Increase the age of my tamagotchi every x minutes. Once pet hits certain age, make him morph to next level pet....css?
+
+//animate pet somehow with JQuery perhaps. If i make my character look like Kirby i can expand the circle of his body back and forth.
+
+//--------------------
+
+//Also increase the hunger, boredom, sleepiness every x minutes
+
+//Display these on the page so user can see^^^^^^^Alert
+
+//Give the user a warning if these levels are getting high
+
+//If user clicks a button that feeds, sleeps, plays. Then decrease the number. 10 is the max.
+
+//if any of those metrics get to 10. Tamagotchi dies
+
+//maybe turn color of low levels to red
+
+//--------------------
+
+//Display how many days the tamagotchi has been alive
+
+//Probably create a function for time passing, and have all of the properties increase my x minutes.
+
+//If user does not complete any button clicks within a certain amount of time display a message saying tamagotchi has died.
+
+//Display message of cause of death.
+
+//reset game after user clicks a restart button
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
